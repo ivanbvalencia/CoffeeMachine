43ee7ab4bb3772fe673d7714b43576673365a79d
ivanbvalencia <ivanbvalencia@yahoo.com>
2026-06-22 16:06:02 +0800

    Commit

--- CoffeeMachine/Program.cs
+++ CoffeeMachine/Program.cs
@@ -17,14 +17,8 @@ builder.Services.AddHttpClient<IWeatherService, WeatherService>();
 
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-}
 
 app.MapGet("/brew-coffee", async (ICoffeeService service) =>
 {
